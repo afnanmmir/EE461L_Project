1e20270344dd88c96da46d72251caf30afdf40fe
Afnan Mir <mirafnan12@gmail.com>
2022-03-21 15:13:39 -0500

    comments

--- backend/server/routes/main.py
+++ backend/server/routes/main.py
@@ -20,5 +20,9 @@ def home():
 @main.route('/protected')
 @jwt_required()
 def protected():
+    """
+    This is the template used for when an authentication is needed. This is what would be needed for when we create the project 
+    blueprint
+    """
     current_user = get_jwt_identity()
     return current_user
