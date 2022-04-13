3168ea8f5e2b76b12625f85cd447eb1a5e5d28ae
JaxonCoward <jaxoncoward@gmail.com>
2022-04-13 12:57:38 -0500

    Added hardware backend fns

--- backend/server/routes/hardware.py
+++ backend/server/routes/hardware.py
@@ -14,6 +14,7 @@ hardware = Blueprint('hardware', __name__)
 encoder = JSONEncoder()
 
 database = mongo.db
+hw_collection = database['hardware'] # Accesses the user collection of the database
 
 @hardware.route('/', methods=["POST"])
 def create_hardware():
@@ -40,7 +41,38 @@ def create_hardware():
     """
     # function be able to create a new hardware set and add to the database.
     # function should check to make sure the request sent has a json of proper format
-    return
+
+    req = request.get_json()
+    hw_name = req['HWSetName']
+    hw = hw_collection.find_one({'HWSetName':hw_name})
+    print(f"Hardware Set {hw}")
+    if(hw_collection == None):
+        doc = {
+                "HWSetName": req["HWSetName"],
+                "total_quantity": req["total_quantity"],
+                "available": req["total_quantity"],
+                "checked_out": 0
+            }
+        hw_collection.insert_one(doc)
+        # print("This worked first time!")
+        return {
+            'message': "Hardware Set Created"
+        }, 201
+    else:
+        if(hw):
+            return "Hardware Set with this name already exists", 422
+        else:
+            doc = {
+                "HWSetName": req["HWSetName"],
+                "total_quantity": req["total_quantity"],
+                "available": req["total_quantity"],
+                "checked_out": 0
+            }
+            hw_collection.insert_one(doc)
+            # print("This worked!")
+            return {
+                'message': "Hardware Set Created"
+            }, 201
 
 @hardware.route('/', methods=["GET"])
 def get_all_hardware():
@@ -58,5 +90,11 @@ def get_all_hardware():
     201: all hardware sets in the database
     """
     # Function should return list of all hardware sets in the database
-    return
+    req = request.get_json()
+    hw_list = []
+
+    for hw in hw_collection.find():
+        hw_list.append(hw)
+
+    return hw_list, 201
 
