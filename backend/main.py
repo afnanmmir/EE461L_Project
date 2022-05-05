979462b63eb755c437c88b1f47c8a4bab900deb7
Afnan Mir <mirafnan12@gmail.com>
2022-05-05 01:54:13 -0500

    Test

--- backend/main.py
+++ backend/main.py
@@ -8,4 +8,4 @@ from server import createApp # imports the createApp function from the server fo
 app = createApp()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=80)
